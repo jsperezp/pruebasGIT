0051cf6195c92b8230870beb8a960bc8d574c01e
Jhony <jhonatanstiven127@gmail.com>
2020-07-15 15:32:26 -0500

    Adicion de Switch para seleccion de mes

--- Bienvenida.py
+++ Bienvenida.py
@@ -10,5 +10,36 @@ anio = int(anioNacimiento)
 mes = int(mesNacimiento)
 dia = int(diaNacimiento)
 
-print("Bienvenido",name)
-print("Usted tiene",2020-anio,"años",7-mes,"meses y",15-dia,"días")
+String monthName;
+
+switch(mes){
+	case 1: monthName = 'Enero';
+			break;
+	case 2: monthName = 'Febrero';
+			break;
+	case 3: monthName = 'Marzo';
+			break;
+	case 4: monthName = 'Abril';
+			break;
+	case 5: monthName = 'Mayo';
+			break;
+	case 6: monthName = 'Junio';
+			break;
+	case 7: monthName = 'Julio';
+			break;
+	case 8: monthName = 'Agosto';
+			break;
+	case 9: monthName = 'Septiembre';
+			break;			
+	case 10: monhtName = 'Octubre';
+			break;
+	case 11: monthName = 'Noviembre';
+			break;
+	case 12: monthName = 'Diciembre';
+			break;
+	default: monthName = 'Mes Invalido';
+			break;
+}
+print("Bienvenido",name);
+print("Usted tiene",2020-anio,"años",7-mes,"meses y",15-dia,"días");
+print("Nacio en el mes de",monthName);
