12e81bec0d9d68d3c85580e2b0c0923be718662e
Jhony <jhonatanstiven127@gmail.com>
2020-07-15 15:59:18 -0500

    Adicion de Switch al codigo - Nueva imagen2

--- Bienvenida.py
+++ Bienvenida.py
@@ -10,36 +10,25 @@ anio = int(anioNacimiento)
 mes = int(mesNacimiento)
 dia = int(diaNacimiento)
 
-String monthName;
+monthName = '';
 
-switch(mes){
-	case 1: monthName = 'Enero';
-			break;
-	case 2: monthName = 'Febrero';
-			break;
-	case 3: monthName = 'Marzo';
-			break;
-	case 4: monthName = 'Abril';
-			break;
-	case 5: monthName = 'Mayo';
-			break;
-	case 6: monthName = 'Junio';
-			break;
-	case 7: monthName = 'Julio';
-			break;
-	case 8: monthName = 'Agosto';
-			break;
-	case 9: monthName = 'Septiembre';
-			break;			
-	case 10: monhtName = 'Octubre';
-			break;
-	case 11: monthName = 'Noviembre';
-			break;
-	case 12: monthName = 'Diciembre';
-			break;
-	default: monthName = 'Mes Invalido';
-			break;
-}
+def nombreMes(i):
+	switcher = {
+		1: 'Enero',
+		2: 'Febrero',
+		3: 'Marzo',				
+		4: 'Abril',				
+		5: 'Mayo',			
+		6: 'Junio',				
+		7: 'Julio',				
+		8: 'Agosto',				
+		9: 'Septiembre',						
+		10: 'Octubre',			
+		11: 'Noviembre',				
+		12: 'Diciembre',			
+	}
+	return switcher.get(i, "Invalido");	
+	
 print("Bienvenido",name);
 print("Usted tiene",2020-anio,"años",7-mes,"meses y",15-dia,"días");
-print("Nacio en el mes de",monthName);
+print("Nacio en el mes de:",nombreMes(mes));
