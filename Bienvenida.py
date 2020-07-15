f9c94e6b3dd0dbdfa0eae41d31de6208ac932806
Jhony <jhonatanstiven127@gmail.com>
2020-07-15 15:22:45 -0500

    primerGuardado

---
+++ Bienvenida.py
@@ -0,0 +1,14 @@
+nombre = "Jhonatan"
+apellido = "Perez"
+edad = 33
+name = input("Ingrese su nombre: ")
+anioNacimiento = input("Ingrese su año de nacimiento: ")
+mesNacimiento = input("Ingrese su mes de nacimiento: ")
+diaNacimiento = input("Ingrese su día de nacimiento: ")
+
+anio = int(anioNacimiento)
+mes = int(mesNacimiento)
+dia = int(diaNacimiento)
+
+print("Bienvenido",name)
+print("Usted tiene",2020-anio,"años",7-mes,"meses y",15-dia,"días")
